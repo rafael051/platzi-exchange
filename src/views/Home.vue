4c4344fe4ed19840e99f245a6842fb096b7fd332
Rafael Gonzalez <rafagonzadi99@gmail.com>
2019-12-12 22:07:04 -0500

    Prueba proyecto terminado

---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <bounce-loader :loading="isLoading" :color="'#68d391'" :size="100" />
+    <px-assets-table v-if="!isLoading" :assets="assets" />
+  </div>
+</template>
+
+<script>
+import PxAssetsTable from '@/components/PxAssetsTable'
+import api from '@/api'
+
+export default {
+  name: 'Home',
+  components: { PxAssetsTable },
+
+  data() {
+    return {
+      isLoading: true,
+      assets: []
+    }
+  },
+
+  created() {
+    api
+      .getAssets()
+      .then(assets => (this.assets = assets))
+      .finally(() => (this.isLoading = false))
+  }
+}
+</script>
