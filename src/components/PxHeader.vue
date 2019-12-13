0a7d95003039173f3d0654ffd3c38a6ea433581a
Rafael Gonzalez <rafagonzadi99@gmail.com>
2019-12-12 23:04:20 -0500

    Prueba segundo commit

--- src/components/PxHeader.vue
+++ src/components/PxHeader.vue
@@ -5,22 +5,18 @@
         <div class="flex items-center flex-shrink-0 text-white mr-6">
           <px-icon class="mr-2" />
           <router-link
-            :to="{ name: 'home' }"
+            :to="{ name: 'Home' }"
             class="font-semibold text-xl tracking-tight"
-            >Platzi Exchange</router-link
-          >
+          >Platzi Exchange</router-link>
         </div>
-        <div
-          class="hidden sm:block w-full block flex-grow lg:flex lg:items-center lg:w-auto"
-        >
+        <div class="hidden sm:block w-full block flex-grow lg:flex lg:items-center lg:w-auto">
           <div class="text-sm lg:flex-grow">
             <router-link
               v-for="l in links"
               :key="l.title"
               :to="l.to"
               class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
-              >{{ l.title }}</router-link
-            >
+            >{{ l.title }}</router-link>
           </div>
         </div>
       </nav>
